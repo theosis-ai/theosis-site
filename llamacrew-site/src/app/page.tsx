791c47c364ff322b4efab2e0fee394919602b5d8
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2024-05-27 10:28:10 -0400

    add twitter link

--- llamacrew-site/src/app/page.tsx
+++ llamacrew-site/src/app/page.tsx
@@ -15,7 +15,8 @@ export default function Component() {
           Llama and LangChain powered agent teams
         `}
           </p>
-          <Link className="flex items-center justify-center" href="https://github.com/LlamaCrew">
+          <div className="flex justify-center gap-4">
+          <Link className="flex" href="https://github.com/LlamaCrew">
             <svg 
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -31,6 +32,22 @@ export default function Component() {
               <path d="M9 18c-4.51 2-5-2-7-2" />
             </svg>
           </Link>
+          <Link className="flex" href="https://x.com/LlamaCrewAi">
+              <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+            </svg>
+            </Link>
+            </div>
         </section>
       </main>
     </div>
