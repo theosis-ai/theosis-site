2d9959555f18cb900c72dd18a36ff05856cf977f
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2024-05-26 20:01:04 -0400

    add types

--- llamacrew-site/src/app/layout.tsx
+++ llamacrew-site/src/app/layout.tsx
@@ -1,9 +1,14 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
 
+import { ReactNode } from "react";
 import { Roboto } from 'next/font/google'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
+interface LayoutProps {
+  children: ReactNode
+};
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -18,7 +23,7 @@ const inter = Inter({
   weight: ['300']
 })
 
-export default function Layout({ children }) {
+export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body className={roboto.variable + ' ' + inter.variable}>
