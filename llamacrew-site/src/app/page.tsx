6e209d4767b1250fe63873f13ea45c048f0ed054
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2024-06-01 09:43:48 -0400

    update

--- llamacrew-site/src/app/page.tsx
+++ llamacrew-site/src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
       <main className="flex-1 flex flex-col items-center justify-center px-4 md:px-6">
         <section className="max-w-2xl text-center space-y-6">
           <h1 className="text-4xl font-roboto tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-            LlamaCrew
+            AGNTK
           </h1>
           <p className="text-lg text-gray-900">
             {`
-          Llama and LangChain powered agent teams
+          Meta, Cohere, and LangChain powered agents
         `}
           </p>
           <div className="flex justify-center gap-4">
-          <Link className="flex" href="https://github.com/LlamaCrew">
+          <Link className="flex" href="https://github.com/agntk">
             <svg 
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -32,21 +32,6 @@ export default function Home() {
               <path d="M9 18c-4.51 2-5-2-7-2" />
             </svg>
           </Link>
-          <Link className="flex" href="https://x.com/LlamaCrewAi">
-              <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
-            </svg>
-            </Link>
             </div>
         </section>
       </main>
