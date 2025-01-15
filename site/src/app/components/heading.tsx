8fb56186c70ebb965253f9fb8401a34f1574807f
jxtngx <26209687+jxtngx@users.noreply.github.com>
2025-01-15 11:32:21 -0500

    update heading

--- site/src/app/components/heading.tsx
+++ site/src/app/components/heading.tsx
@@ -1,11 +1,14 @@
 export default function Heading() {
     return (
-        <section className="max-w-2xl font-mono text-center space-y-4">
-            <h1 className="max-w-[400px] mx-auto text-4xl font-medium tracking-tight text-gray-900 sm:text-2xl md:text-6xl">
+        <section className="max-w-2xl font-mono text-center">
+            <h1 className="max-w-[400px] mx-auto text-4xl font-medium tracking-tight text-gray-900 sm:text-2xl md:text-6xl mb-4">
                 Theosis AI
             </h1>
-            <p className="max-w-[550px] mx-auto text-md tracking-tight text-neutral-500 sm:text-md md:text-md">
+            <p className="max-w-[550px] mx-auto text-md tracking-tight text-neutral-500 sm:text-md md:text-md mb-1.5">
               Exploring the Frontiers of Artificial Intelligence
+            </p>
+            <p className="text-xs tracking-tight text-neutral-500">
+              a portfolio of work by Justin Goheen
             </p>
         </section>
     );
