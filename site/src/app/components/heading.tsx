cc64f8c2c68fdd5d1f674b13dc3c41e19e35c755
jxtngx <26209687+jxtngx@users.noreply.github.com>
2025-01-31 15:41:04 -0500

    fix: update heading component styles and adjust GitHub icon size

--- site/src/app/components/heading.tsx
+++ site/src/app/components/heading.tsx
@@ -1,7 +1,7 @@
 export default function Heading() {
     return (
-        <section className="max-w-2xl font-mono text-center">
-            <h1 className="max-w-[400px] mx-auto text-4xl tracking-wide text-gray-900 sm:text-2xl md:text-6xl mb-4">
+        <section>
+            <h1 className="max-w-[400px] font-mono text-center text-6xl mx-auto tracking-wider text-gray-900 sm:text-4xl md:text-6xl">
                 Theosis AI
             </h1>
             {/* <p className="max-w-[550px] mx-auto text-md tracking-tight text-neutral-500 sm:text-md md:text-md mb-1.5">
--- site/src/app/components/ui/github.tsx
+++ site/src/app/components/ui/github.tsx
@@ -2,8 +2,8 @@ export default function GitHub () {
     return (
         <svg 
         xmlns="http://www.w3.org/2000/svg" 
-        width="30" 
-        height="30" 
+        width="32" 
+        height="32" 
         viewBox="0 0 24 24" 
         fill="none" 
         stroke="currentColor" 
