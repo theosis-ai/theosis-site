c2bc6495812eebd84c03b87bcf7ef4043a703f54
jxtngx <26209687+jxtngx@users.noreply.github.com>
2025-02-12 12:31:06 -0500

    fix: remove authentication components from footer and nav

--- packages/ui/src/components/nav.tsx
+++ packages/ui/src/components/nav.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { LogIn } from "lucide-react";
 import ModeSwitcher from "@workspace/ui/components/modeSwitcher";
 import { House } from 'lucide-react';
 
 export default function Nav() {
-  const pathname = usePathname();
-  if (pathname === "/lab") return null;
 
   return (
     <nav className="flex items-center justify-between mt-8 ml-4 mr-4 mb-4 sm:ml-12 sm:mr-12">
@@ -23,30 +18,10 @@ export default function Nav() {
         <Link className="items-center flex gap-1" href="/blog">
           Blog
         </Link>
-        {/* <SignedIn>
-          <Link className="items-center flex gap-1" href="/lab">
-            Tune Lab
-          </Link>
-        </SignedIn> */}
       </div>
       <div className="flex flex-1 justify-end">
         <ModeSwitcher />
       </div>
-      {/* <div className="flex flex-1 justify-end">
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton>
-            <Link
-              href="/sign-in"
-              className="btn flex gap-2 justify-center items-center"
-            >
-              Sign In <LogIn height="18" width="18" />
-            </Link>
-          </SignInButton>
-        </SignedOut>
-      </div> */}
     </nav>
   );
 }
